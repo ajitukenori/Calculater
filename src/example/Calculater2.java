a33a05814a4aa01cd471570ce67c1116ab43cced
MatsuyamaAkira <matsuyama220061@eggnet.ac.jp>
2023-07-13 16:15:39 +0900

    second commit

---
+++ src/example/Calculater2.java
@@ -0,0 +1,16 @@
+package example;
+
+//import java.util.Scanner;
+
+public class Calculater2 {
+	//Scanner scanner = new Scanner(System.in);
+	
+	void first() {
+		System.out.println("一番目の数を入力してください");
+		
+	}
+	
+	void second() {
+		System.out.println("二番目の数を入力してください");
+	}
+}
